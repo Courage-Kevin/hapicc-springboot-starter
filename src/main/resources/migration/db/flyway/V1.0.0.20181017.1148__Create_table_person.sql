013a3ac3a57755d477591295c9f12941acd6ae75
vveicc <vveicc@163.com>
2018-10-17 22:31:47 +0800

    Add Flyway and Liquibase

---
+++ src/main/resources/migration/db/flyway/V1.0.0.20181017.1148__Create_table_person.sql
@@ -0,0 +1,5 @@
+CREATE TABLE person (
+  id   BIGINT(20)   NOT NULL AUTO_INCREMENT,
+  name VARCHAR(256) NOT NULL,
+  primary key (id)
+);
---
+++ src/main/resources/migration/db/flyway/V1.0.0.20181017.1207__Insert_into_person.sql
@@ -0,0 +1,8 @@
+INSERT INTO person (name)
+VALUE ('琼恩');
+
+INSERT INTO person (name)
+VALUE ('丹妮');
+
+INSERT INTO person (name)
+VALUE ('影魔');
